e7545d1bcdabb638af90cace87df60444cde7bc1
Michal Skoryk <michal.skoryk@gmail.com>
2026-05-18 23:28:56 +0200

    initial commit

---
+++ src/main/java/com/skoryk/projects/meetler/MeetlerApplication.java
@@ -0,0 +1,13 @@
+package com.skoryk.projects.meetler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skoryk/projects/meetler/MeetlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skoryk.projects.meetler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
